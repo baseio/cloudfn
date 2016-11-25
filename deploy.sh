5d311f946ee7c6de9143ffe41b6688aa6aa16f70
Jorgen Skogmo <js@base.io>
2016-11-25 14:01:07 +0100

    0.0.16

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ SERVER="cloudfn.stream"
 RDIR="/var/www/cloudfn-system"
 VERSION="$(node -pe 'require("./package.json").version')"
 
-git add --all .
+git add .
 git commit -a -m "$VERSION" --quiet
 git push origin master --quiet
 npm version patch
