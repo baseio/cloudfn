a39449ed843a120793d27383d09fbd08ff8c2f2f
Jorgen Skogmo <js@base.io>
2016-12-08 22:46:25 +0100

    0.1.26

--- plugins/core/send.js
+++ plugins/core/send.js
@@ -5,18 +5,9 @@ module.exports = function(data){
 	//console.log("@send() ", this.args);
 
 	// obey return format from url-query
-	console.log("@core.send use provided format?", this.args);
-	if( this.args.query.format ){
-		switch (this.args.query.format) {
-			case 'jsonp' : 
-				console.log("@core.send", "sending jsonp");
-				this.res.set('Content-Type', 'text/javascript');
-				this.res.send( 'var jsonp='+ JSON.stringify(data) );
-				break;
-		}
-	}else if( this.args.query.callback ){
+	if( this.args.query.callback ){
 
-		console.log("@core.send", "sending callback");
+		console.log("@core.send", "sending jsonp");
 		this.res.set('Content-Type', 'text/javascript');
 		this.res.send( this.args.query.callback +'('+ JSON.stringify(data) +');');
 
