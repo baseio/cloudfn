da89aecd6424685e2194f02b107855284fe6d82a
Jorgen Skogmo <js@base.io>
2016-12-12 22:07:22 +0100

    0.1.29

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/@/ls/:user/:hash', (req, res) => {
 
     let list = [];
     if( cloudfn.tasks.list[req.params.user] ){
-        list = Object.keys( cloudfn.tasks.list[req.params.user]);
+        list = Object.keys( cloudfn.tasks.list[req.params.user]).map( (scriptname) => req.params.user +'/'+ scriptname);
     }
 
     send_msg(res, "ls", list );
